963396ef90858f404c25a919646a7e19b34c7544
Mathieu Pigois <mpigois@yahoo.fr>
2018-06-09 12:27:53 +0200

    decoupage fork

---
+++ src/board/Board_fork.cpp
@@ -0,0 +1,69 @@
+#include <vector>
+#include <boost/algorithm/string.hpp>
+#include <iomanip>
+#include <iostream>
+
+#include "Board.hpp"
+
+#include "../common/Common.hpp"
+#include "AMove.hpp"
+
+using namespace std;
+using namespace Common;
+
+        std::vector <string> Board::SearchForkPositions(Color color) {
+        // fourchette = piece sur une case ou plusieurs pieces adverses sont attaquées
+            
+        // algo 1 : fourchettes niveau 1 (déclinable niveau 2)
+            // parcourir les pieces de la couleur et chercher les coups possibles
+            // puis regarder apres chaque coup combien il y a de prises possibles
+            // si plusieurs prises possibles alors la case intermédiaire est une case de fourchette
+            // NON IMPLEMENTE
+            
+        // algo 2 : cases de fourchette (pas de notion de niveau pour l'atteindre)
+            // partir des pieces adverses et tagguer les cases d'ou peuvent venir chaque type de pieces
+            // si une case a plusieurs tags de meme piece, c'est une case de fourchette
+            
+            int cf[6][8][8];
+            for (int i=0; i<8; i++) {
+                for (int j=0; j<8; j++) {
+                    if (IsPieceAdverse(i,j)) {
+                        
+                        // pour chaque type de piece
+                        for (int t=0; t<6; t++) {
+                            // chercher les cases a portée de [i][j]
+                            /*
+                             for (each case a portée de i,j) {
+                                // les tagguer
+                                cf[t][x][y] ++;
+                            }*/
+                        }
+                    }
+                }
+            }
+            
+            std::vector <string> pos_list;
+            for (int i=0; i<8; i++) {
+                for (int j=0; j<8; j++) { 
+                    for (int t=0; t<6; t++) {
+                        if (cf[t][i][j] >= 2) {
+                            pos_list.push_back(Position(i,j));
+                        }
+                    }
+                }
+            }
+                
+        return pos_list;
+    }
+    
+        std::vector <AMove> Board::SearchForkAttackMoves(Color color, string pos) {
+                std::vector <AMove> moves;
+            
+            return moves;
+    }
+    
+        std::vector <AMove> Board::SearchForkDefenseMoves(Color color, string pos) {
+                std::vector <AMove> moves;
+            
+            return moves;
+    }
